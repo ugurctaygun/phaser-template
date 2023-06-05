5956b26c3a5fe44005203fadb7a69e693234c37a
ugurctaygun <ugurctaygun@gmail.com>
2023-06-05 14:42:38 +0300

    loading tiled json to scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,14 +2,16 @@ import Phaser from "phaser";
 
 class Play extends Phaser.Scene {
   constructor() {
-    super("Play");
+    super("PlayScene");
   }
-  preload() {
-    this.load.image("sky", "assets/sky.png");
-  }
+  preload() {}
 
   create() {
-    this.add.image(0, 0, "sky").setOrigin(0);
+    const map = this.make.tilemap({ key: "map" });
+    const tileset1 = map.addTilesetImage("main_lev_build_1", "tiles-1");
+    const tileset2 = map.addTilesetImage("main_lev_build_2", "tiles-2");
+    map.createStaticLayer("environment", tileset1);
+    map.createStaticLayer("platforms", tileset1);
   }
 }
 
