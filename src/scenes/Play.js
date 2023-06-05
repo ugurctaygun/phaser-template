7bb8082792198da35353b91af722acb8ec885371
ugurctaygun <ugurctaygun@gmail.com>
2023-06-05 13:56:03 +0300

    webpack boilerplate

---
+++ src/scenes/Play.js
@@ -0,0 +1,16 @@
+import Phaser from "phaser";
+
+class Play extends Phaser.Scene {
+  constructor() {
+    super("Play");
+  }
+  preload() {
+    this.load.image("sky", "assets/sky.png");
+  }
+
+  create() {
+    this.add.image(0, 0, "sky").setOrigin(0);
+  }
+}
+
+export default Play;
